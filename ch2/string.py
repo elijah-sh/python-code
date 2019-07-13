1adaaf93dec905058c1f9c437114eb900cd65aa5
沈帅虎 <shuaihu.shen@hand-china.com>
2019-07-13 23:27:11 +0800

    [ADD] 对字符串的基础操作

---
+++ ch2/string.py
@@ -0,0 +1,10 @@
+message = "Hello Python World! "
+print(message.title())
+print(message.lower())
+print(message.upper())
+print(message.upper() + "\n" + message.lower())
+print(message.upper() + "\t" + message.lower())
+message = " Hello Python World! "
+print(message.rstrip())
+print(message.lstrip())
+print(message.strip())
