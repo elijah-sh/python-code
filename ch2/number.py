8beffac34796c77faa219a0c57e26fc4f2a1e275
沈帅虎 <shuaihu.shen@hand-china.com>
2019-07-15 00:03:09 +0800

    [ADD] 对字符串的数字基础操作

---
+++ ch2/number.py
@@ -0,0 +1,40 @@
+# 数字相关知识和操作
+
+# 整数
+# 一个* 是乘  ** 是平方
+print(3**2)
+print(3 ** 3)
+print((2+3)*4)
+print(6/2*(1+2))
+
+# 浮点数
+print("======================")
+print("==========浮点型=======")
+print(0.1 + 0.1)
+print(0.2 + 0.2)
+print(0.1 + 0.2)
+print(2 * 0.2)
+print(0.1 * 0.2)
+print(0.2 * 0.2)
+print(3 * 0.2)
+
+# 函数str() 避免类型错误 只有用age会报错
+print("======================")
+print("==========str()=======")
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+# 练习
+print("======================")
+print("==========练习=======")
+# 练习1 用加减乘除 输入8
+print(1 + 7)
+print(10 - 2)
+print(4 * 2)
+print(2 ** 3)
+print(24 / 3)
+# 练习2 将最喜欢的数据 加入到一句话中并打印
+love_nummber = 777
+love_message = "I favorite number is" + str(love_nummber)
+print(love_message)
