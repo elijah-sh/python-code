694abb0f02c7fa0bdd83a3df395b0747d951c445
沈帅虎 <shuaihu.shen@hand-china.com>
2019-07-15 23:18:29 +0800

    [ADD] 对列表的认识

---
+++ ch3/bicycles.py
@@ -0,0 +1,14 @@
+# 列表介绍
+print("======================")
+print("==========列表介绍=======")
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialied']
+print("bicycles: " + str(bicycles))
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])
+message = "My first bicycle was a " + bicycles[0].title() + '.'
+print(message)
+
