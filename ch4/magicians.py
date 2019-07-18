9483658afdb8d571e066616a0b1f797303c983ef
沈帅虎 <shuaihu.shen@hand-china.com>
2019-07-18 23:24:34 +0800

    [ADD] for循环

---
+++ ch4/magicians.py
@@ -0,0 +1,22 @@
+# 操作列表
+print("======================")
+print("==========操作列表=======")
+
+# 4.1 遍历整个列表
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can`t wait to see you next trick, "+ magician.title() + ". \n")
+
+print("Thank you, everyone. That was a great magic show!")
+
+
+# 练习
+print("======================")
+print("==========练习=======")
+
+pizzas = ['煎饼果子', '肉夹馍', '考冷面', '手抓饼']
+for pizza in pizzas:
+    print("I like " + pizza)
+
+print("I really love Chinese food")
